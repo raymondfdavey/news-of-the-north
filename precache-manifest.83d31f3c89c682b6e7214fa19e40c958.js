5bfb74c37126ea945738dd05e74ad39c0a00b303
rfd <rfd@rfds-MacBook-Air.local>
2021-07-01 23:04:34 +0100

    Updates

---
+++ precache-manifest.83d31f3c89c682b6e7214fa19e40c958.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25747c2ccc763babcdb90b18beb68acd",
+    "url": "/news-of-the-north/index.html"
+  },
+  {
+    "revision": "167417dfe99925da6beb",
+    "url": "/news-of-the-north/static/css/main.a3eaf6ba.chunk.css"
+  },
+  {
+    "revision": "063c42decdea8d204d8a",
+    "url": "/news-of-the-north/static/js/2.637cc818.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/news-of-the-north/static/js/2.637cc818.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "167417dfe99925da6beb",
+    "url": "/news-of-the-north/static/js/main.2c5d8d52.chunk.js"
+  },
+  {
+    "revision": "75b8dd71a07818732236",
+    "url": "/news-of-the-north/static/js/runtime-main.db7f442d.js"
+  },
+  {
+    "revision": "92e2bea7310592b507b0dda1fb0f9284",
+    "url": "/news-of-the-north/static/media/down.92e2bea7.png"
+  },
+  {
+    "revision": "995e52e1ec64c53e8b4818ef6afe6610",
+    "url": "/news-of-the-north/static/media/up.995e52e1.png"
+  }
+]);
